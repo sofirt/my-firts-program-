88022799ea570876cdc2217a479d19482d0fabf5
sofirt <113055255+sofirt@users.noreply.github.com>
2022-09-07 14:11:04 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.showString("arroz con pollo ")
+basic.showIcon(IconNames.Happy)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.forever(function () {
+	
+})
